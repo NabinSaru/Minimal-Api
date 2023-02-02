e9dbb3d5492d6cf9d1eceae910b368001da2a471
Nabin Saru <jenexthegreat@gmail.com>
2023-02-02 23:08:47 +0545

    chore: remove unused codes

--- Minimal Api/Program.cs
+++ Minimal Api/Program.cs
@@ -2,40 +2,14 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
 using System.Security.Claims;
 
+const string AuthSchema = "cookie";
+
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDataProtection();
-//builder.Services.AddHttpContextAccessor();
-//builder.Services.AddScoped<AuthService>();
-
 builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie");
+    .AddCookie(AuthSchema);
 
 var app = builder.Build();
-
-//app.Use((ctx, next) =>
-//{
-//    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-//    var protector = idp.CreateProtector("auth-cookie");
-
-//    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-//    var protectedPayload = authCookie.Split("=").Last();
-//    var decryptedPayload = protector.Unprotect(protectedPayload);
-//    var payload = decryptedPayload.Split(":");
-//    var key = payload[0];
-//    var value = payload[1];
-
-//    // similar to name, age
-//    var claims = new List<Claim>();
-//    claims.Add(new Claim(key, value));
-
-//    // identity equivalent to passport and claim maybe password id
-//    var identity = new ClaimsIdentity(claims);
-//    // claims principal is the authority like license accessing driving authority
-//    ctx.User = new ClaimsPrincipal(identity);
-
-//    return next();
-//});
 
 app.UseAuthentication();
 
@@ -50,8 +24,6 @@ app.MapGet("/access", (HttpContext ctx) =>
 
 app.MapGet("/login", async (HttpContext ctx) =>
 {
-    //auth.SignIn();
-
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "john"));
     var identity = new ClaimsIdentity(claims, "cookie");
@@ -63,20 +35,3 @@ app.MapGet("/login", async (HttpContext ctx) =>
 
 app.Run();
 
-//public class AuthService
-//{
-//    private readonly IDataProtectionProvider _idp;
-//    private readonly IHttpContextAccessor _accessor;
-
-//    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
-//    {
-//        _idp = idp;
-//        _accessor = accessor;
-//    }
-
-//    public void SignIn()
-//    {
-//        var protector = _idp.CreateProtector("auth-cookie");
-//        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr: john")}";
-//    }
-//}
