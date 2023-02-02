99653aea5c9cf98b1ab6e481f4bb1617d11bd26b
Nabin Saru <jenexthegreat@gmail.com>
2023-02-02 23:05:12 +0545

    feat: replace the authentication middleware with the ones provided by dotnet

--- Minimal Api/Program.cs
+++ Minimal Api/Program.cs
@@ -1,37 +1,43 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.DataProtection;
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDataProtection();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<AuthService>();
+//builder.Services.AddDataProtection();
+//builder.Services.AddHttpContextAccessor();
+//builder.Services.AddScoped<AuthService>();
+
+builder.Services.AddAuthentication("cookie")
+    .AddCookie("cookie");
 
 var app = builder.Build();
 
-app.Use((ctx, next) =>
-{
-    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-    var protector = idp.CreateProtector("auth-cookie");
+//app.Use((ctx, next) =>
+//{
+//    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+//    var protector = idp.CreateProtector("auth-cookie");
 
-    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-    var protectedPayload = authCookie.Split("=").Last();
-    var decryptedPayload = protector.Unprotect(protectedPayload);
-    var payload = decryptedPayload.Split(":");
-    var key = payload[0];
-    var value = payload[1];
+//    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+//    var protectedPayload = authCookie.Split("=").Last();
+//    var decryptedPayload = protector.Unprotect(protectedPayload);
+//    var payload = decryptedPayload.Split(":");
+//    var key = payload[0];
+//    var value = payload[1];
 
-    // similar to name, age
-    var claims = new List<Claim>();
-    claims.Add(new Claim(key, value));
+//    // similar to name, age
+//    var claims = new List<Claim>();
+//    claims.Add(new Claim(key, value));
 
-    // identity equivalent to passport and claim maybe password id
-    var identity = new ClaimsIdentity(claims);
-    // claims principal is the authority like license accessing driving authority
-    ctx.User = new ClaimsPrincipal(identity);
+//    // identity equivalent to passport and claim maybe password id
+//    var identity = new ClaimsIdentity(claims);
+//    // claims principal is the authority like license accessing driving authority
+//    ctx.User = new ClaimsPrincipal(identity);
 
-    return next();
-});
+//    return next();
+//});
+
+app.UseAuthentication();
 
 app.MapGet("/", () => {
     return "welcome to minimal api";
@@ -42,28 +48,35 @@ app.MapGet("/access", (HttpContext ctx) =>
     return ctx.User.FindFirst("usr").Value;
 });
 
-app.MapGet("/login", (AuthService auth) =>
+app.MapGet("/login", async (HttpContext ctx) =>
 {
-    auth.SignIn();
+    //auth.SignIn();
+
+    var claims = new List<Claim>();
+    claims.Add(new Claim("usr", "john"));
+    var identity = new ClaimsIdentity(claims, "cookie");
+    var user = new ClaimsPrincipal(identity);
+
+    await ctx.SignInAsync("cookie", user);
     return "Ok";
 });
 
 app.Run();
 
-public class AuthService
-{
-    private readonly IDataProtectionProvider _idp;
-    private readonly IHttpContextAccessor _accessor;
+//public class AuthService
+//{
+//    private readonly IDataProtectionProvider _idp;
+//    private readonly IHttpContextAccessor _accessor;
 
-    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
-    {
-        _idp = idp;
-        _accessor = accessor;
-    }
+//    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+//    {
+//        _idp = idp;
+//        _accessor = accessor;
+//    }
 
-    public void SignIn()
-    {
-        var protector = _idp.CreateProtector("auth-cookie");
-        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr: john")}";
-    }
-}
+//    public void SignIn()
+//    {
+//        var protector = _idp.CreateProtector("auth-cookie");
+//        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr: john")}";
+//    }
+//}
