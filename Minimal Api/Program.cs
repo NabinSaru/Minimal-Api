0cd814610ed12182a9652b56a082be97a7f505e9
Nabin Saru <jenexthegreat@gmail.com>
2023-02-02 21:54:42 +0545

    feat: basic cookie authentication setup

--- Minimal Api/Program.cs
+++ Minimal Api/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/access", (HttpContext ctx) =>
+{
+    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+    var authPayload = authCookie.Split("=").Last();
+    var payload = authCookie.Split(":");
+    var key = payload[0];
+    var value = payload[1];
+    return value;
+});
+
+app.MapGet("/login", (HttpContext ctx) =>
+{
+    ctx.Response.Headers["set-cookie"] = "auth=usr: john";
+    return "Ok";
+});
 
 app.Run();
