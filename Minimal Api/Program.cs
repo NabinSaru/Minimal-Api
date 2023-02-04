ea5040ac6ff13d4b5a248452884f910e81ff12a9
Nabin Saru <jenexthegreat@gmail.com>
2023-02-04 14:35:36 +0545

    feat: add an end point isAdmin to demonstrate the authorization mechanism

--- Minimal Api/Program.cs
+++ Minimal Api/Program.cs
@@ -19,13 +19,30 @@ app.MapGet("/", () => {
 
 app.MapGet("/access", (HttpContext ctx) =>
 {
-    return ctx.User.FindFirst("usr").Value;
+    return ctx.User.FindFirst("usr")?.Value ?? "empty";
+});
+
+app.MapGet("/isAdmin", (HttpContext ctx) =>
+{
+    if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthSchema))
+    {
+        ctx.Response.StatusCode = 401;
+        return false;
+
+    }
+    if (!ctx.User.HasClaim("role", "admin"))
+    {
+        ctx.Response.StatusCode = 403;
+        return false;
+    }
+    return true;
 });
 
 app.MapGet("/login", async (HttpContext ctx) =>
 {
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "john"));
+    claims.Add(new Claim("role", "admin"));
     var identity = new ClaimsIdentity(claims, "cookie");
     var user = new ClaimsPrincipal(identity);
 
