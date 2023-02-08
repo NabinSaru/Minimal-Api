69a3a456644ae706c2d8cab54f1e309f5845613a
Nabin Saru <jenexthegreat@gmail.com>
2023-02-08 20:36:41 +0545

    feat: add cookie authentcation and authorization for the local visitor
    Added the cookie authentcation and authorization for the local visitor and cookie authentication handler for the visitor type.

--- Minimal Api/Program.cs
+++ Minimal Api/Program.cs
@@ -1,82 +1,76 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDataProtection();
-//builder.Services.AddHttpContextAccessor();
-//builder.Services.AddScoped<AuthService>();
-
 builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie");
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", options => {})
+    .AddCookie("local");
+
+builder.Services.AddAuthorization(builder =>
+{
+    builder.AddPolicy("customer", policy =>
+    {
+        policy.AddAuthenticationSchemes("local", "visitor")
+            .RequireAuthenticatedUser();
+    });
+});
 
 var app = builder.Build();
 
-//app.Use((ctx, next) =>
-//{
-//    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-//    var protector = idp.CreateProtector("auth-cookie");
-
-//    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-//    var protectedPayload = authCookie.Split("=").Last();
-//    var decryptedPayload = protector.Unprotect(protectedPayload);
-//    var payload = decryptedPayload.Split(":");
-//    var key = payload[0];
-//    var value = payload[1];
-
-//    // similar to name, age
-//    var claims = new List<Claim>();
-//    claims.Add(new Claim(key, value));
-
-//    // identity equivalent to passport and claim maybe password id
-//    var identity = new ClaimsIdentity(claims);
-//    // claims principal is the authority like license accessing driving authority
-//    ctx.User = new ClaimsPrincipal(identity);
-
-//    return next();
-//});
-
 app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", () => {
-    return "welcome to minimal api";
-});
+app.MapGet("/", () => Task.FromResult("welcome to minimal api"));
 
 app.MapGet("/access", (HttpContext ctx) =>
 {
     return ctx.User.FindFirst("usr").Value;
 });
 
-app.MapGet("/login", async (HttpContext ctx) =>
+app.MapGet("/login-local", async (HttpContext ctx) =>
 {
-    //auth.SignIn();
-
     var claims = new List<Claim>();
-    claims.Add(new Claim("usr", "john"));
+    claims.Add(new Claim("usr", "jen"));
     var identity = new ClaimsIdentity(claims, "cookie");
     var user = new ClaimsPrincipal(identity);
 
     await ctx.SignInAsync("cookie", user);
-    return "Ok";
 });
 
 app.Run();
 
-//public class AuthService
-//{
-//    private readonly IDataProtectionProvider _idp;
-//    private readonly IHttpContextAccessor _accessor;
+public class VisitorAuthHandler: CookieAuthenticationHandler
+{
+    public VisitorAuthHandler(
+        IOptionsMonitor<CookieAuthenticationOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    {
+    }
 
-//    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
-//    {
-//        _idp = idp;
-//        _accessor = accessor;
-//    }
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var result = await base.HandleAuthenticateAsync();
 
-//    public void SignIn()
-//    {
-//        var protector = _idp.CreateProtector("auth-cookie");
-//        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr: john")}";
-//    }
-//}
+        if (result.Succeeded)
+        {
+            return result;
+        }
+
+        var claims = new List<Claim>();
+        claims.Add(new Claim("usr", "jen"));
+        var identiy = new ClaimsIdentity(claims, "visitor");
+        var user = new ClaimsPrincipal(identiy);
+
+        await Context.SignInAsync("visitor", user);
+
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
+}
